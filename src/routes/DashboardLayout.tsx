6fc161d099375b2fbc677c8e0fb142cf488de2de
YoichiDev-tech <yoichi_dev@proton.me>
2026-09-08 15:07:07 +0100

    fixed wrong import

--- src/routes/DashboardLayout.tsx
+++ src/routes/DashboardLayout.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 
 import Sidebar from '../components/layout/Sidebar';
-import TopBar from '../components/layout/TopBar';
+import TopBar from '../components/layout/Topbar';
 import { track } from '../lib/track';
 import { startSession, endSession, recordScroll } from '../lib/session';
 
